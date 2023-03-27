d2c64cf54cac96e3518273cf8317bf24d7e76df9
mzaedan <zaedanm108@gmail.com>
2023-03-27 11:24:58 +0700

    update

--- resources/views/buku/create.blade.php
+++ resources/views/buku/create.blade.php
@@ -7,57 +7,77 @@
     <div class="card-header">
         <h2 class="card-title">Tambah Buku</h2>
     </div>
-
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
     
     <div class="card-body">
-    <form action="{{ url('/update') }}" method="post" enctype="multipart/form-data">
-
+      <form action="#" method="post" enctype="multipart/form-data">
         @csrf
 
        <div class="row">
           <div class="col-xs-12 col-sm-12 col-md-12">
               <div class="form-group">
-                  <strong>Nama Penulis</strong>
+                  <strong>Judul Buku:</strong>
                   <input type="text" name="nama" class="form-control" placeholder="" autofocus>
               </div>
           </div>
           <div class="col-xs-12 col-sm-12 col-md-12">
               <div class="form-group">
-                  <strong>Alamat</strong>
-                  <textarea class="form-control" style="height:150px" name="sinopsis" placeholder=""></textarea>
+                  <strong>Tahun Terbit:</strong>
+                  <input type="text" name="tahun_terbit" class="form-control" placeholder="">
               </div>
           </div>
           <div class="col-xs-12 col-sm-6 col-md-12">
-                <div class="form-group">
-                    <strong>No. Telepon</strong>
-                    <input type="nama" name="text" class="form-control" placeholder="" autofocus>
-                </div>
+              <div class="form-group">
+                  <strong>Penulis:</strong>
+              </div>
+              <div class="form-group" style="height: auto;">
+              <select class="js-example-basic-single" style="width: 100%;" name="id_penulis" id="id_penulis">
+             
+          </select>
           </div>
-          <div class="col-xs-12 col-sm-6 col-md-12">
-                <div class="form-group">
-                    <strong>Email</strong>
-                    <input type="text" name="nama" class="form-control" placeholder="" autofocus>
-                </div>
           </div>
+          <div class="col-xs-12 col-sm-12 col-md-12">
+              <div class="form-group">
+                  <strong>Penerbit:</strong>
+              </div>
+              <div class="form-group">
+              <select class="js-example-basic-single" style="width: 100%;" name="id_penerbit" id="id_penerbit">
+              <option disabled value>Pilih Penerbit</option>
+              
+          </select>
+          </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-12">
+              <div class="form-group">
+                  <strong>Kategori:</strong>
+              </div>
+          <div class="form-group">
+              <select class="js-example-basic-single" style="width: 100%;" name="id_kategori" id="id_kategori">
+              <option disabled value>Pilih Kategori</option>
+              
+          </select>
+          </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-12">
+          <div class="form-group">
+              <strong>Sinopsis:</strong>
+              <textarea class="form-control" style="height:150px" name="sinopsis" placeholder=""></textarea>
+          </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-12">
+          <div class="form-group">
+              <strong>Image:</strong>
+              <input type="file" name="image" class="form-control" placeholder="image">
+          </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-12">
+              <button type="submit" class="btn btn-primary">Simpan</button>
+              <a href="#" class="btn btn-warning">Kembali</a>
           </div>
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-              <button type="submit" class="btn btn-primary">Simpan</button>
-
-              <a href="{{ url('/index') }}" class="btn btn-warning">Kembali</a>
-
-          </div>
-
-</form>
+      </form>
+    </div>
+  </div>
 </div>
+
+
 @endsection
